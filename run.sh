7d5ddd7b7a68afb0e703c8a3b2dd261de767bbaa
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-04-19 16:35:32 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+chmod +x *.*
+chmod +x verifyta
+../verifyta -o1 -u verification1-avoid.xml > results/avoid1.txt
+../verifyta -o1 -u verification1-reach.xml > results/reach2.txt
+../verifyta -o1 -u verification2-avoid.xml > results/avoid2.txt
+../verifyta -o1 -u verification2-reach.xml > results/reach2.txt
+../verifyta -o1 -u verification3-avoid-phase1.xml > results/avoid3-1.txt
+../verifyta -o1 -u verification3-avoid-phase2.xml > results/avoid3-2.txt
+../verifyta -o1 -u verification3-avoid-phase3.xml > results/avoid3-3.txt
+../verifyta -o1 -u verification3-reach-phase1.xml > results/reach3-1.txt
+../verifyta -o1 -u verification3-reach-phase2.xml > results/reach3-2.txt
+../verifyta -o1 -u verification3-reach-phase3.xml > results/reach3-3.txt
+../verifyta -o1 -u verification4-avoid.xml > results/avoid4.txt
+../verifyta -o1 -u verification4-reach.xml > results/reach4.txt
+../verifyta -o1 -u verification5-avoid.xml > results/avoid5.txt
+../verifyta -o1 -u verification5-reach.xml > results/reach5.txt
+../verifyta -o1 -u verification6-avoid.xml > results/avoid6.txt
+../verifyta -o1 -u verification6-reach.xml > results/reach6.txt
