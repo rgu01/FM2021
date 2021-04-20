280a2504aa7114508e69e24f64dd20c84072ecae
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-04-20 08:30:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 ../verifyta -o1 -u verification1-avoid.xml > results/avoid1.txt
-../verifyta -o1 -u verification1-reach.xml > results/reach2.txt
+../verifyta -o1 -u verification1-reach.xml > results/reach1.txt
 ../verifyta -o1 -u verification2-avoid.xml > results/avoid2.txt
 ../verifyta -o1 -u verification2-reach.xml > results/reach2.txt
 ../verifyta -o1 -u verification3-avoid-phase1.xml > results/avoid3-1.txt
